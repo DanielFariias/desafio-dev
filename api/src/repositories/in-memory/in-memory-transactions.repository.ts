310a999618916e80d10b497d7741360b2cd5a82b
DanielFariias <danielfariias15@gmail.com>
2025-05-11 15:19:58 -0300

    feat(core): adiciona InMemoryTransactionsRepository para testes isolados de Transactions

---
+++ api/src/repositories/in-memory/in-memory-transactions.repository.ts
@@ -0,0 +1,45 @@
+import { TransactionsRepository } from '../transactions.repository';
+import { Transaction } from '@prisma/client';
+import { randomUUID } from 'crypto';
+import {
+  CreateTransactionParams,
+  FindTransactionByUniqueKeyParams,
+} from '../../types/transaction';
+
+export class InMemoryTransactionsRepository implements TransactionsRepository {
+  private transactions: Transaction[] = [];
+
+  async findByUniqueKey({
+    storeId,
+    transactionAt,
+    card,
+    cpf,
+    type,
+  }: FindTransactionByUniqueKeyParams) {
+    return (
+      this.transactions.find(
+        (tx) =>
+          tx.storeId === storeId &&
+          tx.transactionAt.getTime() === transactionAt.getTime() &&
+          tx.card === card &&
+          tx.cpf === cpf &&
+          tx.type === type,
+      ) ?? null
+    );
+  }
+
+  async create(data: CreateTransactionParams) {
+    const transaction: Transaction = {
+      id: randomUUID(),
+      storeId: data.storeId,
+      type: data.type,
+      transactionAt: data.transactionAt,
+      value: data.value,
+      cpf: data.cpf,
+      card: data.card,
+    };
+
+    this.transactions.push(transaction);
+    return transaction;
+  }
+}
