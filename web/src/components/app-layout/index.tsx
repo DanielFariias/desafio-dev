8f45c5c201d35b668ebaf05cfc638f36069304c6
DanielFariias <danielfariias15@gmail.com>
2025-05-12 00:19:19 -0300

    feat(web): adiciona Footer

--- web/src/components/app-layout/index.tsx
+++ web/src/components/app-layout/index.tsx
@@ -2,6 +2,7 @@ import type React from 'react';
 
 import { Header } from '../header';
 import styles from './styles.module.scss';
+import { Footer } from '../footer';
 
 interface AppLayoutProps {
   children: React.ReactNode;
@@ -12,6 +13,7 @@ export function AppLayout({ children }: AppLayoutProps) {
     <div className={styles.wrapper}>
       <Header />
       <main className={styles.content}>{children}</main>
+      <Footer />
     </div>
   );
 }
---
+++ web/src/components/footer/index.tsx
@@ -0,0 +1,10 @@
+import styles from './styles.module.scss';
+
+export function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p>CNAB Web © 2025 - Todos os direitos reservados.</p>
+      <p>Versão 1.0.0</p>
+    </footer>
+  );
+}
